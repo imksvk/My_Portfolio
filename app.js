a3e24aa259fa88c38571ed5a6d1aaff4335a0f42
imksvk <keshavkumar123456789101112@gmail.com>
2025-04-18 11:07:01 +0530

    update darkmode

--- app.js
+++ app.js
@@ -4,3 +4,21 @@ function toggleMenu(){
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+
+
+let darkmodebtn = document.querySelector(".darkmodebtn");
+let mode = 1;
+
+
+darkmodebtn.addEventListener("click",()=>{
+   if(mode == 1){
+       darkmodebtn.innerHTML = `<i class="ri-moon-line"></i>`;
+        mode = 0;
+    }
+    else{
+        darkmodebtn.innerHTML = `<i class="ri-moon-fill"></i>`;
+        mode = 1;
+    }
+    document.body.classList.toggle("darkmode");
+})
